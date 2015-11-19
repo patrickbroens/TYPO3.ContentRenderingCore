653f5a8c0087ed7e66d45a9f75ec50c135ee66a7
Patrick Broens <patrick@patrickbroens.nl>
2015-11-19 12:22:47 +0100

    [TASK] Added CType table functionality
    In the CE Table som fields have moved from FlexForm to regular DB fields.
    
    This patch adds all configuration for these fields

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -21,48 +21,81 @@ call_user_func(function () {
     $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['default'] = 'textmedia';
 
     $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['textmedia'] = 'mimetypes-x-content-text-media';
+
     $GLOBALS['TCA']['tt_content']['palettes']['mediaAdjustments'] = [
         'showitem' => '
-			imagewidth;' . $languageFilePrefix . 'tt_content.palette.textmedia.imagewidth,
-			imageheight;' . $languageFilePrefix . 'tt_content.palette.textmedia.imageheight,
-			imageborder;' . $languageFilePrefix . 'tt_content.palette.textmedia.imageborder
-		'
+            imagewidth;' . $languageFilePrefix . 'tt_content.palette.textmedia.imagewidth,
+            imageheight;' . $languageFilePrefix . 'tt_content.palette.textmedia.imageheight,
+            imageborder;' . $languageFilePrefix . 'tt_content.palette.textmedia.imageborder
+        '
     ];
+
     $GLOBALS['TCA']['tt_content']['palettes']['gallerySettings'] = [
         'showitem' => '
-			imageorient;' . $frontendLanguageFilePrefix . 'imageorient_formlabel,
-			imagecols;' . $frontendLanguageFilePrefix . 'imagecols_formlabel
-		'
+            imageorient;' . $frontendLanguageFilePrefix . 'imageorient_formlabel,
+            imagecols;' . $frontendLanguageFilePrefix . 'imagecols_formlabel
+        '
     ];
+
+    $GLOBALS['TCA']['tt_content']['palettes']['tableconfiguration'] = [
+        'showitem' => '
+            table_delimiter,
+            table_enclosure
+        '
+    ];
+
+    $GLOBALS['TCA']['tt_content']['palettes']['tablelayout'] = [
+        'showitem' => '
+            cols,
+            table_header_position,
+            table_tfoot
+        '
+    ];
+
+    $GLOBALS['TCA']['tt_content']['types']['table'] = [
+        'showitem' => '
+                --palette--;' . $frontendLanguageFilePrefix . 'palette.general;general,
+                --palette--;' . $frontendLanguageFilePrefix . 'palette.header;header,rowDescription,
+                bodytext;;;nowrap:wizards[table];' . $frontendLanguageFilePrefix . 'field.table.bodytext,
+                --palette--;;tableconfiguration,
+                table_caption,
+            --div--;' . $frontendLanguageFilePrefix . 'tabs.appearance,
+                layout;' . $frontendLanguageFilePrefix . 'layout_formlabel,
+                --palette--;' . $frontendLanguageFilePrefix . 'palette.table_layout;tablelayout,
+                --palette--;' . $frontendLanguageFilePrefix . 'palette.appearanceLinks;appearanceLinks,
+            --div--;' . $frontendLanguageFilePrefix . 'tabs.access,
+                hidden;' . $frontendLanguageFilePrefix . 'field.default.hidden,
+                --palette--;' . $frontendLanguageFilePrefix . 'palette.access;access,
+            --div--;' . $frontendLanguageFilePrefix . 'tabs.extended
+        '
+    ];
+
     $GLOBALS['TCA']['tt_content']['types']['textmedia'] = [
         'showitem' => '
-				--palette--;' . $frontendLanguageFilePrefix . 'palette.general;general,
-				--palette--;' . $frontendLanguageFilePrefix . 'palette.header;header,
-				bodytext;' . $frontendLanguageFilePrefix . 'bodytext_formlabel;;richtext:rte_transform[mode=ts_css],
-			--div--;' . $frontendLanguageFilePrefix . 'tabs.media,
-				assets,
-				--palette--;' . $frontendLanguageFilePrefix . 'palette.imagelinks;imagelinks,
-			--div--;' . $frontendLanguageFilePrefix . 'tabs.appearance,
-				layout;' . $frontendLanguageFilePrefix . 'layout_formlabel,
-				--palette--;' . $languageFilePrefix . 'tt_content.palette.mediaAdjustments;mediaAdjustments,
-				--palette--;' . $languageFilePrefix . 'tt_content.palette.gallerySettings;gallerySettings,
-				--palette--;' . $frontendLanguageFilePrefix . 'palette.appearanceLinks;appearanceLinks,
-			--div--;' . $frontendLanguageFilePrefix . 'tabs.access,
-				hidden;' . $frontendLanguageFilePrefix . 'field.default.hidden,
-				--palette--;' . $frontendLanguageFilePrefix . 'palette.access;access,
-			--div--;' . $frontendLanguageFilePrefix . 'tabs.extended
-		',
+                --palette--;' . $frontendLanguageFilePrefix . 'palette.general;general,
+                --palette--;' . $frontendLanguageFilePrefix . 'palette.header;header,
+                bodytext;' . $frontendLanguageFilePrefix . 'bodytext_formlabel;;richtext:rte_transform[mode=ts_css],
+            --div--;' . $frontendLanguageFilePrefix . 'tabs.media,
+                assets,
+                --palette--;' . $frontendLanguageFilePrefix . 'palette.imagelinks;imagelinks,
+            --div--;' . $frontendLanguageFilePrefix . 'tabs.appearance,
+                layout;' . $frontendLanguageFilePrefix . 'layout_formlabel,
+                --palette--;' . $languageFilePrefix . 'tt_content.palette.mediaAdjustments;mediaAdjustments,
+                --palette--;' . $languageFilePrefix . 'tt_content.palette.gallerySettings;gallerySettings,
+                --palette--;' . $frontendLanguageFilePrefix . 'palette.appearanceLinks;appearanceLinks,
+            --div--;' . $frontendLanguageFilePrefix . 'tabs.access,
+                hidden;' . $frontendLanguageFilePrefix . 'field.default.hidden,
+                --palette--;' . $frontendLanguageFilePrefix . 'palette.access;access,
+            --div--;' . $frontendLanguageFilePrefix . 'tabs.extended
+        ',
     ];
 
     // Add category tab when categories column exits
     if (!empty($GLOBALS['TCA']['tt_content']['columns']['categories'])) {
         $GLOBALS['TCA']['tt_content']['types']['textmedia']['showitem'] .=
         ',--div--;LLL:EXT:lang/locallang_tca.xlf:sys_category.tabs.category,
-				categories';
+                categories';
     }
-
-    // Add table wizard
-    $GLOBALS['TCA']['tt_content']['types']['table']['columnsOverrides']['bodytext']['defaultExtras'] = 'nowrap:wizards[table]';
 
     // Add additional fields for bullets + upload CTypes
     $additionalColumns = [
@@ -116,6 +149,68 @@ call_user_func(function () {
                 'foreign_types' => $GLOBALS['TCA']['tt_content']['columns']['image']['config']['foreign_types']
             ], $GLOBALS['TYPO3_CONF_VARS']['SYS']['mediafile_ext'])
         ],
+        'table_caption' => [
+            'exclude' => true,
+            'label' => $languageFilePrefix . 'tt_content.table_caption',
+            'config' => [
+                'type' => 'input'
+            ]
+        ],
+        'table_delimiter' => [
+            'exclude' => true,
+            'label' => $languageFilePrefix . 'tt_content.table_delimiter',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    [$languageFilePrefix . 'tt_content.table_delimiter.124', 124],
+                    [$languageFilePrefix . 'tt_content.table_delimiter.59', 59],
+                    [$languageFilePrefix . 'tt_content.table_delimiter.44', 44],
+                    [$languageFilePrefix . 'tt_content.table_delimiter.58', 58],
+                    [$languageFilePrefix . 'tt_content.table_delimiter.9', 9]
+                ],
+                'default' => 124
+            ]
+        ],
+        'table_enclosure' => [
+            'exclude' => true,
+            'label' => $languageFilePrefix . 'tt_content.table_enclosure',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    [$languageFilePrefix . 'tt_content.table_enclosure.0', 0],
+                    [$languageFilePrefix . 'tt_content.table_enclosure.39', 39],
+                    [$languageFilePrefix . 'tt_content.table_enclosure.34', 34]
+                ],
+                'default' => 0
+            ]
+        ],
+        'table_header_position' => [
+            'exclude' => true,
+            'label' => $languageFilePrefix . 'tt_content.table_header_position',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    [$languageFilePrefix . 'tt_content.table_header_position.0', 0],
+                    [$languageFilePrefix . 'tt_content.table_header_position.1', 1],
+                    [$languageFilePrefix . 'tt_content.table_header_position.2', 2]
+                ],
+                'default' => 0
+            ]
+        ],
+        'table_tfoot' => [
+            'exclude' => true,
+            'label' => $languageFilePrefix . 'tt_content.table_tfoot',
+            'config' => [
+                'type' => 'check',
+                'default' => 0,
+                'items' => [
+                    ['LLL:EXT:lang/locallang_core.xml:labels.enabled', 1]
+                ]
+            ]
+        ]
     ];
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $additionalColumns);
--- ext_tables.sql
+++ ext_tables.sql
@@ -2,8 +2,13 @@
 # Table structure for table 'tt_content'
 #
 CREATE TABLE tt_content (
+	assets int(11) unsigned DEFAULT '0' NOT NULL,
 	bullets_type tinyint(3) unsigned DEFAULT '0' NOT NULL,
+	table_caption varchar(255) DEFAULT NULL,
+	table_delimiter smallint(6) unsigned DEFAULT '0' NOT NULL,
+	table_enclosure smallint(6) unsigned DEFAULT '0' NOT NULL,
+	table_header_position tinyint(3) unsigned DEFAULT '0' NOT NULL,
+	table_tfoot tinyint(1) unsigned DEFAULT '0' NOT NULL,
 	uploads_description tinyint(1) unsigned DEFAULT '0' NOT NULL,
 	uploads_type tinyint(3) unsigned DEFAULT '0' NOT NULL,
-	assets int(11) unsigned DEFAULT '0' NOT NULL,
 );
