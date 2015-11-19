fcfb3b9d42f06a3cf11eff090c990bac26251cba
Patrick Broens <patrick@patrickbroens.nl>
2015-11-19 16:57:54 +0100

    [TASK] Update title and description of extension

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Content Rendering Core',
+    'title' => 'Backport for fluid_styled_content',
     'description' => 'Content rendering by Fluid templates',
     'category' => 'fe',
     'version' => '1.0.0',
